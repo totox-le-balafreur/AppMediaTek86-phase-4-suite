da650ab0666f2c918cf40ecad071b19740cea0b4
Thomas <spacelord559@hotmail.com>
2025-05-27 15:41:11 +0200

    phase 4 frm connection fini frm connection fini frm gestion fini frm supprmbre finiAjoutez des fichiers projet.

--- AppMediaTek86/view/FrmAjoutsMbre.Designer.cs
+++ AppMediaTek86/view/FrmAjoutsMbre.Designer.cs
@@ -36,11 +36,15 @@
             this.btnAffMbre = new System.Windows.Forms.Button();
             this.btnRetour = new System.Windows.Forms.Button();
             this.cbBoxLstMbre = new System.Windows.Forms.ComboBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblPrenom = new System.Windows.Forms.Label();
             this.grpBoxAjouts.SuspendLayout();
             this.SuspendLayout();
             // 
             // grpBoxAjouts
             // 
+            this.grpBoxAjouts.Controls.Add(this.lblPrenom);
+            this.grpBoxAjouts.Controls.Add(this.lblNom);
             this.grpBoxAjouts.Controls.Add(this.btnEnregistrer);
             this.grpBoxAjouts.Controls.Add(this.cbBoxAffect);
             this.grpBoxAjouts.Controls.Add(this.txtBoxPrenom);
@@ -61,6 +65,7 @@
             this.btnEnregistrer.TabIndex = 3;
             this.btnEnregistrer.Text = "Enregistrer";
             this.btnEnregistrer.UseVisualStyleBackColor = true;
+            this.btnEnregistrer.Click += new System.EventHandler(this.btnEnregistrer_Click);
             // 
             // cbBoxAffect
             // 
@@ -73,19 +78,17 @@
             // 
             // txtBoxPrenom
             // 
-            this.txtBoxPrenom.Location = new System.Drawing.Point(7, 47);
+            this.txtBoxPrenom.Location = new System.Drawing.Point(60, 47);
             this.txtBoxPrenom.Name = "txtBoxPrenom";
             this.txtBoxPrenom.Size = new System.Drawing.Size(113, 20);
             this.txtBoxPrenom.TabIndex = 1;
-            this.txtBoxPrenom.Text = "Prenom";
             // 
             // txtBoxNom
             // 
-            this.txtBoxNom.Location = new System.Drawing.Point(7, 20);
+            this.txtBoxNom.Location = new System.Drawing.Point(60, 21);
             this.txtBoxNom.Name = "txtBoxNom";
             this.txtBoxNom.Size = new System.Drawing.Size(113, 20);
             this.txtBoxNom.TabIndex = 0;
-            this.txtBoxNom.Text = "Nom";
             // 
             // btnAffMbre
             // 
@@ -95,6 +98,7 @@
             this.btnAffMbre.TabIndex = 1;
             this.btnAffMbre.Text = "Afficher membres";
             this.btnAffMbre.UseVisualStyleBackColor = true;
+            this.btnAffMbre.Click += new System.EventHandler(this.btnAffMbre_Click);
             // 
             // btnRetour
             // 
@@ -104,6 +108,7 @@
             this.btnRetour.TabIndex = 2;
             this.btnRetour.Text = "retour";
             this.btnRetour.UseVisualStyleBackColor = true;
+            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
             // 
             // cbBoxLstMbre
             // 
@@ -113,8 +118,27 @@
             this.cbBoxLstMbre.Size = new System.Drawing.Size(166, 21);
             this.cbBoxLstMbre.TabIndex = 3;
             this.cbBoxLstMbre.Text = "Liste des membres";
+            this.cbBoxLstMbre.Visible = false;
             // 
-            // AjoutsMbre
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(6, 24);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(29, 13);
+            this.lblNom.TabIndex = 4;
+            this.lblNom.Text = "Nom";
+            // 
+            // lblPrenom
+            // 
+            this.lblPrenom.AutoSize = true;
+            this.lblPrenom.Location = new System.Drawing.Point(6, 50);
+            this.lblPrenom.Name = "lblPrenom";
+            this.lblPrenom.Size = new System.Drawing.Size(43, 13);
+            this.lblPrenom.TabIndex = 5;
+            this.lblPrenom.Text = "Prenom";
+            // 
+            // FrmAjoutsMbre
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -123,7 +147,7 @@
             this.Controls.Add(this.btnRetour);
             this.Controls.Add(this.btnAffMbre);
             this.Controls.Add(this.grpBoxAjouts);
-            this.Name = "AjoutsMbre";
+            this.Name = "FrmAjoutsMbre";
             this.Text = "Ajouts membres";
             this.grpBoxAjouts.ResumeLayout(false);
             this.grpBoxAjouts.PerformLayout();
@@ -141,5 +165,7 @@
         private System.Windows.Forms.Button btnAffMbre;
         private System.Windows.Forms.Button btnRetour;
         private System.Windows.Forms.ComboBox cbBoxLstMbre;
+        private System.Windows.Forms.Label lblPrenom;
+        private System.Windows.Forms.Label lblNom;
     }
 }
--- AppMediaTek86/view/FrmAjoutsMbre.cs
+++ AppMediaTek86/view/FrmAjoutsMbre.cs
@@ -1,4 +1,5 @@
 ﻿using AppMediaTek86.controller;
+using AppMediaTek86.model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,11 +12,123 @@ using System.Windows.Forms;
 
 namespace AppMediaTek86.view
 {
+    /// <summary>
+    /// fenetre qui gere l'ajout de personnel
+    /// </summary>
     public partial class FrmAjoutsMbre : Form
     {
+        /// <summary>
+        /// Controleur de la fenetre
+        /// </summary>
+        FrmAjoutsMbreController controller;
+        /// <summary>
+        /// objet pour gerer la liste du personnel
+        /// </summary>
+        private BindingSource bdgMembre;
+        /// <summary>
+        /// objet pour gerer la liste des services
+        /// </summary>
+        private BindingSource bdgService;
+        /// <summary>
+        ///construction des composants graphiques et appel des autre initialisation
+        /// </summary>
         public FrmAjoutsMbre()
         {
             InitializeComponent();
+            Init();
+            RemplirListeDuPersonnel();
+            RemplirListeService();
+            // On abonne les événements
+            txtBoxNom.TextChanged += ChampsModifies;
+            txtBoxPrenom.TextChanged += ChampsModifies;
+            cbBoxAffect.SelectedIndexChanged += ChampsModifies;
+
+            // On fait une vérification initiale
+            VerifierChamps();
+        }
+        /// <summary>
+        /// initialise les composants
+        /// </summary>
+        private void Init() 
+        {
+            bdgMembre = new BindingSource();
+            controller = new FrmAjoutsMbreController();
+            bdgService = new BindingSource();
+        }
+        /// <summary>
+        /// affiche la liste du personnel
+        /// </summary>
+        private void RemplirListeDuPersonnel()
+        {
+            List<Personnel> lePersonnel = controller.GetLePersonnel();
+            bdgMembre.DataSource = lePersonnel;
+            cbBoxLstMbre.DataSource = bdgMembre;
+            cbBoxLstMbre.DisplayMember = "NomComplet";
+            cbBoxLstMbre.ValueMember = "IdPersonnel";
+        }
+        /// <summary>
+        /// affiche la liste des services
+        /// </summary>
+        private void RemplirListeService()
+        {
+            List<Service> lesServices = controller.GetLesServices();
+            bdgService.DataSource = lesServices;
+            cbBoxAffect.DataSource = bdgService;
+            cbBoxAffect.DisplayMember = "Nom";
+            cbBoxAffect.ValueMember = "Idservice";
+        }
+        /// <summary>
+        /// vérifie si tous les champs obligatoires sont remplis
+        /// </summary>
+        private void VerifierChamps()
+        {
+            btnEnregistrer.Enabled =
+                !string.IsNullOrWhiteSpace(txtBoxNom.Text) &&
+                !string.IsNullOrWhiteSpace(txtBoxPrenom.Text) &&
+                cbBoxAffect.SelectedIndex != -1;
+        }
+        /// <summary>
+        /// appelé à chaque modification des champs
+        /// </summary>
+        private void ChampsModifies(object sender, EventArgs e)
+        {
+            VerifierChamps();
+        }
+        /// <summary>
+        /// gere l'affichage de la liste du personnel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnAffMbre_Click(object sender, EventArgs e)
+        {
+            cbBoxLstMbre.Visible = true;
+        }
+        /// <summary>
+        /// Demande d'enregistrement de l'ajout ou de la modification d'un membre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <summary>
+        /// gere la fermeture de la fenetre
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            // ferme la fenetre
+            this.Close();
+        }
+
+        private void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            if (!txtBoxNom.Text.Equals("") && !txtBoxPrenom.Text.Equals("") && cbBoxAffect.SelectedIndex != -1)
+            {
+                Service service = (Service)cbBoxAffect.SelectedItem;
+                Personnel personnel = new Personnel(0, txtBoxNom.Text, txtBoxPrenom.Text,"","", service);
+                controller.AjPersonnel(personnel);
+                RemplirListeDuPersonnel();
+                btnEnregistrer.Enabled = true;
+            }
         }
     }
 }
